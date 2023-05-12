9a6bc87f0dc069a7749a8cd6a21c768a59cdeb96
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-12 16:03:25 -0400

    Aplicando thunk para Peticiones

--- ReactRedux/practica-redux-react/src/components/buscador/BuscadorPokemon.jsx
+++ ReactRedux/practica-redux-react/src/components/buscador/BuscadorPokemon.jsx
@@ -1,4 +1,12 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import fetchPokemon from '../../redux/actions/buscadorAction';
+
 const BuscadorPokemon = () => {
+  //componente para invocar accioens useDispatch
+  const dispatch = useDispatch();
+  //estado para los nombres del pokemon
+  const [pokemon_name, set_Pokemon_name] = useState('mewtwo');
   return (
     <div className='form-group'>
       <label htmlFor='buscar_pokemon' className='text-white'>
@@ -8,9 +16,21 @@ const BuscadorPokemon = () => {
         type='text'
         className='form-control'
         id='buscar_pokemon'
-        value='pikachu'
+        value={pokemon_name}
+        onChange={(event) => {
+          set_Pokemon_name(event.target.value);
+        }}
       />
-      <button className='btn btn-primary mt-3'>Enviar</button>
+      <button
+        className='btn btn-primary mt-3'
+        onClick={() => {
+          //la funcion fetchPokemon donde esta los dispatcher
+          //para hacer la peticion  minuto 3:30 ep.19
+          dispatch(fetchPokemon(pokemon_name));
+        }}
+      >
+        Enviar
+      </button>
     </div>
   );
 };
--- ReactRedux/practica-redux-react/src/redux/reducers/store.js
+++ ReactRedux/practica-redux-react/src/redux/reducers/store.js
@@ -1,7 +1,12 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducers from './rootReducers';
 //para ver el store en el navegador. este es un middleware
 import { composeWithDevTools } from 'redux-devtools-extension';
-const store = createStore(rootReducers, composeWithDevTools());
+import thunk from 'redux-thunk';
+//agregado applymiddleware thunk para  hacer peticion
+const store = createStore(
+  rootReducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
