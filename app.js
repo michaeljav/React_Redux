b2edf7be66c369bba1f2b08fc35f2c8af9ba8f4d
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-11 22:59:50 -0400

    buy and return yoshi video 5

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const { createStore } = require('redux');
 //##### Actions
 const BUY_POCKEMON = 'BUY_POKEMON';
 const RETURN_POCKEMON = 'RETURN_POKEMON';
+const BUY_YOSHI = 'BUY_YOSHI';
+const RETURN_YOSHI = 'RETURN_YOSHI';
 const buy_pokemon_action = (cant) => {
   return {
     type: BUY_POCKEMON,
@@ -15,21 +17,54 @@ const return_pokemon_action = (cant) => {
     payload: cant,
   };
 };
+const buy_yoshi_action = (cant) => {
+  return {
+    type: BUY_YOSHI,
+    payload: cant,
+  };
+};
+const return_yoshi_action = (cant) => {
+  return {
+    type: RETURN_YOSHI,
+    payload: cant,
+  };
+};
 
 //##### Reducers
 const default_games_state = {
   pokemon: 10,
+  yoshi: 10,
 };
 
 const games_reducer = (state = default_games_state, action) => {
   switch (action.type) {
     case BUY_POCKEMON: {
       /*state.pokemon: estado previo - action.payload: estado actual*/
-      return { pokemon: state.pokemon - action.payload };
+      return {
+        ...state,
+        pokemon: state.pokemon - action.payload,
+      };
     }
     case RETURN_POCKEMON: {
       /*state.pokemon: estado previo - action.payload: estado actual*/
-      return { pokemon: state.pokemon + action.payload };
+      return {
+        ...state,
+        pokemon: state.pokemon + action.payload,
+      };
+    }
+    case BUY_YOSHI: {
+      /*state.pokemon: estado previo - action.payload: estado actual*/
+      return {
+        ...state,
+        yoshi: state.yoshi - action.payload,
+      };
+    }
+    case RETURN_YOSHI: {
+      /*state.pokemon: estado previo - action.payload: estado actual*/
+      return {
+        ...state,
+        yoshi: state.yoshi + action.payload,
+      };
     }
     default:
       return state;
@@ -48,3 +83,5 @@ store.subscribe(() => {
 });
 store.dispatch(buy_pokemon_action(3));
 store.dispatch(return_pokemon_action(2));
+store.dispatch(buy_yoshi_action(3));
+store.dispatch(return_yoshi_action(2));
