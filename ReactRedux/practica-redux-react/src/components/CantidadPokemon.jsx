a16fc6b74f763f1ce14a666606505e6b19917737
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-12 11:07:01 -0400

    conect  comp con rdux/usar  state del store vid 13

--- ReactRedux/practica-redux-react/src/components/CantidadPokemon.jsx
+++ ReactRedux/practica-redux-react/src/components/CantidadPokemon.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+//Para conectar los componentes
+import { connect } from 'react-redux';
 
 class CantidadPokemon extends Component {
   render() {
-    return <React.Fragment>Unidades: 30</React.Fragment>;
+    return (
+      <React.Fragment>Unidades: {this.props.game_shop.pokemon}</React.Fragment>
+    );
   }
 }
 
-export default CantidadPokemon;
+//funcion currificada: una funcion que devuelve otra funcion:  connect()()
+//--> en la segunda funcion que esta devolviendo, se
+//pega el componente
+//---> para pasar el estado que esta en el store como props al componente.
+//-Ya arriba podremos acceder a esos props.
+const mapStateToProps = (state) => {
+  return {
+    game_shop: state.game_shop,
+  };
+};
+
+export default connect(mapStateToProps)(CantidadPokemon);
