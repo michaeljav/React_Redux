d7ccfff6b84797be844badba1cd9f1366ed796ef
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-11 22:52:52 -0400

    Buy and return pockemons video 5

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const { createStore } = require('redux');
 
 //##### Actions
 const BUY_POCKEMON = 'BUY_POKEMON';
+const RETURN_POCKEMON = 'RETURN_POKEMON';
 const buy_pokemon_action = (cant) => {
   return {
     type: BUY_POCKEMON,
+    payload: cant,
+  };
+};
+const return_pokemon_action = (cant) => {
+  return {
+    type: RETURN_POCKEMON,
     payload: cant,
   };
 };
@@ -20,6 +27,10 @@ const games_reducer = (state = default_games_state, action) => {
       /*state.pokemon: estado previo - action.payload: estado actual*/
       return { pokemon: state.pokemon - action.payload };
     }
+    case RETURN_POCKEMON: {
+      /*state.pokemon: estado previo - action.payload: estado actual*/
+      return { pokemon: state.pokemon + action.payload };
+    }
     default:
       return state;
   }
@@ -29,9 +40,11 @@ const games_reducer = (state = default_games_state, action) => {
 const store = createStore(games_reducer);
 
 console.log('Estado Inicial', store.getState());
-
 store.subscribe(() => {
-  console.log('Cambio estado desde el subscribe metodo', store.getState());
+  console.log(
+    'Cambio en el store estado desde el subscribe metodo',
+    store.getState()
+  );
 });
-
 store.dispatch(buy_pokemon_action(3));
+store.dispatch(return_pokemon_action(2));
