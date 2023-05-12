384e877e70d238c7ed7fc62c83ef63d522a7e81a
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-12 11:19:42 -0400

    conect comp y exect dispatch vid 14

--- ReactRedux/practica-redux-react/src/components/CompraPokemon.jsx
+++ ReactRedux/practica-redux-react/src/components/CompraPokemon.jsx
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  buy_pokemon_action,
+  return_pokemon_action,
+} from '../redux/actions/gameShopAction';
 
 class CompraPokemon extends Component {
   render() {
     return (
       <div>
-        <button className='btn btn-dark btn-sm '>Comprar Pokemon</button>
-        <button className='btn btn-dark btn-sm '>Regresar Pokemon</button>
+        <button
+          className='btn btn-dark btn-sm '
+          onClick={() => {
+            this.props.buy_pokemon_action(1);
+          }}
+        >
+          Comprar Pokemon
+        </button>
+        <button
+          className='btn btn-dark btn-sm '
+          onClick={() => {
+            this.props.return_pokemon_action(1);
+          }}
+        >
+          Regresar Pokemon
+        </button>
       </div>
     );
   }
 }
 
-export default CompraPokemon;
+//dispatch enviar datos al store
+const mapDispatchtToProps = {
+  buy_pokemon_action,
+  return_pokemon_action,
+};
+//export default CompraPokemon;
+//ya que no usamos los estados, sino que enviarmo datos con el action a travez de ejecutar los dispatch que son lo que  usa la funcion redux para modificar  el store
+///dejamos el primer parametro null
+export default connect(null, mapDispatchtToProps)(CompraPokemon);
--- ReactRedux/practica-redux-react/src/redux/actions/gameShopAction.js
+++ ReactRedux/practica-redux-react/src/redux/actions/gameShopAction.js
@@ -8,7 +8,7 @@ export const buy_pokemon_action = (cant) => {
   };
 };
 
-export const regresar_pokemon_action = (cant) => {
+export const return_pokemon_action = (cant) => {
   return {
     type: RETURN_POKEMON,
     payload: cant,
