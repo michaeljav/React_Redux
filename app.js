8b01367b49ba0810107ec278c1f3d1d1aa4089f9
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-05-11 23:15:19 -0400

    VIDEO 6 -  COMBINE REDUCERS

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const { createStore } = require('redux');
+const { createStore, combineReducers } = require('redux');
 
 //##### Actions
 const BUY_POCKEMON = 'BUY_POKEMON';
 const RETURN_POCKEMON = 'RETURN_POKEMON';
 const BUY_YOSHI = 'BUY_YOSHI';
 const RETURN_YOSHI = 'RETURN_YOSHI';
+
 const buy_pokemon_action = (cant) => {
   return {
     type: BUY_POCKEMON,
@@ -26,6 +27,14 @@ const buy_yoshi_action = (cant) => {
 const return_yoshi_action = (cant) => {
   return {
     type: RETURN_YOSHI,
+    payload: cant,
+  };
+};
+
+const BUY_SWITCH = 'BUY_SWITCH';
+const buy_switch_action = (cant) => {
+  return {
+    type: BUY_SWITCH,
     payload: cant,
   };
 };
@@ -71,8 +80,31 @@ const games_reducer = (state = default_games_state, action) => {
   }
 };
 
+const default_consoles_state = {
+  ps5: 30,
+  switch: 30,
+};
+const consoles_reducer = (state = default_consoles_state, action) => {
+  switch (action.type) {
+    case BUY_SWITCH: {
+      /*state.pokemon: estado previo - action.payload: estado actual*/
+      return {
+        ...state,
+        switch: state.switch - action.payload,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
 //########## Store
-const store = createStore(games_reducer);
+//para usar de dos en adelante reducer hay que usar una funcion de combien reducers
+const rootReducers = combineReducers({
+  games_reducer,
+  consoles_reducer,
+});
+const store = createStore(rootReducers);
 
 console.log('Estado Inicial', store.getState());
 store.subscribe(() => {
@@ -81,7 +113,10 @@ store.subscribe(() => {
     store.getState()
   );
 });
-store.dispatch(buy_pokemon_action(3));
-store.dispatch(return_pokemon_action(2));
-store.dispatch(buy_yoshi_action(3));
-store.dispatch(return_yoshi_action(2));
+
+// store.dispatch(buy_pokemon_action(3));
+// store.dispatch(return_pokemon_action(2));
+// store.dispatch(buy_yoshi_action(3));
+// store.dispatch(return_yoshi_action(2));
+
+store.dispatch(buy_switch_action(3));
